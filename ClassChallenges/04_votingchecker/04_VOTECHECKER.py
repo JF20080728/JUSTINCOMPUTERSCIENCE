4d3145b894fd477a690f8adb4fca4dd05fe500ed
JF20080728 <1037708@pdsb.net>
2025-03-04 09:01:16 -0500

    inital commit

---
+++ ClassChallenges/04_votingchecker/04_VOTECHECKER.py
@@ -0,0 +1,5 @@
+AGE = int(input("Enter your age "))
+if AGE < 18:
+    print("you are unable to vote you are too young")
+if AGE >= 18:
+    print("You are eligible to vote")
---
+++ ClassChallenges/05_Dailyreminder/05_WeeklyActivity.py
@@ -0,0 +1,17 @@
+DAY = input("what is the current day of the week ")
+if DAY == "Monday":
+    print("Start your week with a workout!")
+elif DAY == "Tuesday":
+    print("It's a great day to read a book!")
+elif DAY == "Wednesday":
+    print("Mid-week movie night!")
+elif DAY == "Thursday":
+    print("Try a new recipe!")
+elif DAY == "Friday":
+    print("Relax and enjoy the weekend!")
+elif DAY == "Saturday":
+    print("Go for a hike!")
+elif DAY == "Sunday":
+    print("Prepare for the week ahead with some self-care.")
+else :
+    print("unable to identify the day of the week, please try again")
