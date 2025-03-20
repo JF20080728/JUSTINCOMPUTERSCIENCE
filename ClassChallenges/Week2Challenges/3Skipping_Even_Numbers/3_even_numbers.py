f19ab5eb67f689c955ec6bf2ed71dad30e4e6e85
JF20080728 <1037708@pdsb.net>
2025-03-20 09:21:56 -0400

    inital commit

--- ClassChallenges/Week2Challenges/3Skipping_Even_Numbers/3_even_numbers.py
+++ ClassChallenges/Week2Challenges/3Skipping_Even_Numbers/3_even_numbers.py
@@ -1,12 +1,12 @@
 #-----------------------------------------------------------------------------
 # Name:         Even_Number_skipper
 # Purpose:     write a program that prints numbers from 1 to 10 but skips even numbers
-# Author:      Justin Fung
+# Author:      Justin.F
 # Created:     20-March-2025
 #-----------------------------------------------------------------------------
 
 
 for i in range(1, 11):
     if i  % 2 == 0:
-        print("
+        print(i)
         continue
